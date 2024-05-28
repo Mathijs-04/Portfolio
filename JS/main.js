a014fd7f236a810c9c2ec3e58c53361260d5fbad
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-05-28 18:35:52 +0200

    HTML, CSS & JS Edited

--- JS/main.js
+++ JS/main.js
@@ -9,57 +9,57 @@ function init() {
 
 let projects = [
     {
-        projectTitle: "Gobbo Quest",
+        projectTitle: "Dungeon Defender",
         projectImage: "./IMG/IMG1.webp",
-        projectDescription: "This is the description for Gobbo Quest.",
+        projectDescription: "A 2D Arcade Game made in Excalibur.js",
+        projectText: "This is the text for Dungeon Defender."
+    },
+    {
+        projectTitle: "Gobbo Quest",
+        projectImage: "./IMG/IMG2.webp",
+        projectDescription: "A Top-Down RPG made in Makecode Arcade",
         projectText: "This is the text for Gobbo Quest."
     },
     {
         projectTitle: "Game Magazine",
-        projectImage: "./IMG/IMG2.webp",
-        projectDescription: "This is the description for Game Magazine.",
+        projectImage: "./IMG/IMG3.webp",
+        projectDescription: "An Online Game Magazine made in HTML,CSS and JS",
         projectText: "This is the text for Game Magazine."
     },
     {
         projectTitle: "Denise Kookt!",
-        projectImage: "./IMG/IMG3.webp",
-        projectDescription: "This is the description for Denise Kookt!.",
+        projectImage: "./IMG/IMG4.webp",
+        projectDescription: "An Online Reservation System using a Database",
         projectText: "Denise was altijd al gepassioneerd over koken. Haar liefde voor de keuken begon al op jonge leeftijd, toen ze haar grootmoeder hielp bij het bereiden van traditionele familierecepten. Deze vroege ervaringen legden de basis voor haar culinaire avontuur. Na haar studie aan een prestigieuze kookschool in Frankrijk, keerde Denise terug naar haar geboortestad met een koffer vol nieuwe technieken en een hart vol dromen. Denise opende een klein, gezellig restaurant in het hart van de stad, genaamd \"Denise's Delights\". Hier combineerde ze haar kennis van klassieke Franse keuken met lokale ingrediënten en invloeden. Haar menu wisselde regelmatig, geïnspireerd door de seizoenen en de verse producten die ze op de markt vond. Wat Denise's kookstijl bijzonder maakte, was haar aandacht voor detail en haar liefde voor experimenteren. Ze stond bekend om haar creatieve combinaties, zoals een delicate lavendeltaart met honing van een lokale imker of een hartige venkelsoep geserveerd met een vleugje saffraan. Haar gerechten waren niet alleen een lust voor de smaakpapillen, maar ook voor het oog – prachtig gepresenteerd en altijd met een persoonlijke touch. Naast haar restaurant deelde Denise haar passie via kooklessen en workshops. Mensen van alle leeftijden kwamen samen in haar warme, uitnodigende keuken om te leren en te genieten. Denise geloofde sterk in de verbindende kracht van voedsel en zag elke maaltijd als een kans om mensen samen te brengen. Haar reputatie groeide snel, en al snel werd \"Denise's Delights\" een favoriete bestemming voor zowel locals als toeristen. Denise werd uitgenodigd op kookshows, schreef een populaire kookboekserie en startte zelfs haar eigen YouTube-kanaal, waar ze haar favoriete recepten en kooktips deelde. Ondanks haar succes bleef Denise bescheiden en toegewijd aan haar roots. Ze bleef nieuwe smaken ontdekken en deelde haar kennis en passie met iedereen om haar heen. Voor Denise was koken meer dan een beroep – het was haar manier om liefde en vreugde de wereld in te brengen, één gerecht tegelijk."
     },
     {
         projectTitle: "Reis Rust",
-        projectImage: "./IMG/IMG4.webp",
-        projectDescription: "This is the description for Reis Rust.",
+        projectImage: "./IMG/IMG5.webp",
+        projectDescription: "A simplified route planner for public transport",
         projectText: "This is the text for Reis Rust."
     },
     {
         projectTitle: "Gabber Gandalf",
-        projectImage: "./IMG/IMG5.webp",
-        projectDescription: "This is the description for Gabber Gandalf.",
+        projectImage: "./IMG/IMG6.webp",
+        projectDescription: "A fun side project for a fictional artist",
         projectText: "This is the text for Gabber Gandalf."
     },
     {
         projectTitle: "Game Database",
-        projectImage: "./IMG/IMG6.webp",
-        projectDescription: "This is the description for Game Database.",
+        projectImage: "./IMG/IMG7.webp",
+        projectDescription: "A game Database using a login system",
         projectText: "This is the text for Game Database."
     },
     {
         projectTitle: "Asus Rog Ally",
-        projectImage: "./IMG/IMG7.webp",
-        projectDescription: "This is the description for Asus Rog Ally.",
-        projectText: "This is the text for Asus Rog Ally."
-    },
-    {
-        projectTitle: "Dungeon Defender",
         projectImage: "./IMG/IMG8.webp",
-        projectDescription: "This is the description for Dungeon Defender.",
-        projectText: "This is the text for Dungeon Defender."
+        projectDescription: "A product page for the Asus Rog Ally",
+        projectText: "This is the text for Asus Rog Ally."
     },
     {
         projectTitle: "CLE4 Game",
         projectImage: "./IMG/IMG9.webp",
-        projectDescription: "This is the description for CLE4 Game.",
+        projectDescription: "A 2D Game made with a team",
         projectText: "This is the text for CLE4 Game."
     },
 ];
