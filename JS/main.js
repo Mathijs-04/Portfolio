3c604899e187513120b8ae1a2d6dcee4a98b6835
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-08-13 21:31:28 +0200

    Mobile CSS Edited (WORK IN PROGRESS)

--- JS/main.js
+++ JS/main.js
@@ -34,7 +34,7 @@ let projects = [
         projectTitle: "Unreal Engine 5",
         projectImage: "./IMG/IMG11.webp",
         projectDescription: "Unreal Engine 5 Castle Environment",
-        projectText: "This Unreal Engine 5 environment is my first project in Unreal Engine. I wanted to learn more about Unreal Engine 5 and 3D-Development, so I built this forest environment with castles in it. While making this environment I experimented with various tools and techniques in Unreal Engine 5, like Nanite, Lumen, Landscaping and the Blueprint system. During this project I followed the Unreal Engine 5 Beginner Tutorial by Unreal Sensei.",
+        projectText: "This Unreal Engine 5 environment is my first project in Unreal Engine. I wanted to learn more about Unreal Engine 5 and 3D-Development, so I built this forest environment with castles in it. While making this environment I experimented with various tools and techniques in Unreal Engine 5, like Nanite, Lumen, Landscaping, Modular Design and the UE5 Blueprint system. During this project I followed the Unreal Engine 5 Beginner Tutorial by Unreal Sensei. I highly recommend this free starter course to anyone who wants to learn Unreal Engine 5. You can find the full course on YouTube.",
         projectLink1: "https://www.youtube.com/watch?v=k-zMkzmduqI",
     },
     {
