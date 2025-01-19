19b210e517fbf03fbf77c1b8005ffa9ece1ce7e7
Mathijs-04 <mathijsvandermeijde@gmail.com>
2025-01-19 11:03:04 +0100

    Added new file

---
+++ src/Layout.jsx
@@ -0,0 +1,22 @@
+import { Link, Outlet } from "react-router";
+
+function Layout() {
+    return (
+        <>
+            <header>
+                <h1>Notes App</h1>
+            </header>
+            <nav>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/projects"}>Projects</Link>
+                <Link to={"/experience"}>Experience</Link>
+                <Link to={"/about"}>About</Link>
+            </nav>
+            <main>
+                <Outlet/>
+            </main>
+        </>
+    )
+}
+
+export default Layout;
