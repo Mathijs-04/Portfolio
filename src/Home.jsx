f05954f0d7d219148c60d2e4603721686b5013ae
Mathijs-04 <mathijsvandermeijde@gmail.com>
2025-02-22 14:43:37 +0100

    Added typewriter effect

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,37 @@
 import { useNavigate } from 'react-router';
+import Typewriter from 'typewriter-effect';
 
 function Home() {
     const navigate = useNavigate();
 
     return (
         <>
+            <br/>
             <h1>Mathijs van der Meijde</h1>
-            <h2>Creative Developer</h2>
-            <br />
+            <div id="typewriter">
+                <Typewriter
+                    options={{
+                        loop: true,
+                        delay: 75,
+                    }}
+                    onInit={(typewriter) => {
+                        typewriter
+                            .pauseFor(2500)
+                            .typeString('String <strong>1</strong>')
+                            .pauseFor(300)
+                            .deleteChars(8)
+                            .typeString('String <strong>2</strong>')
+                            .pauseFor(300)
+                            .deleteChars(8)
+                            .typeString('String <strong>3</strong>')
+                            .pauseFor(300)
+                            .deleteChars(8)
+                            .pauseFor(1000)
+                            .start();
+                    }}
+                />
+            </div>
+            <br/>
             <button onClick={() => navigate('/projects')}>Projects</button>
         </>
     );
