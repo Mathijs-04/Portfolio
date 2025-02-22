56f7b30097b99840912fd83401d0a305ead753d5
Mathijs-04 <mathijsvandermeijde@gmail.com>
2025-02-22 13:49:50 +0100

    Added skill blocks

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,18 +1,10 @@
+import Skillsection from "./Skillsection.jsx";
+
 function Experience() {
     return (
         <>
             <h1>Experience</h1>
-            <p>HTML</p>
-            <p>CSS</p>
-            <p>JavaScript</p>
-            <p>PHP</p>
-            <p>React</p>
-            <p>Node.js</p>
-            <p>Express</p>
-            <p>Laravel</p>
-            <p>Tailwind</p>
-            <p>Excalibur.js</p>
-            <p>Unreal Engine 5</p>
+            <Skillsection />
         </>
     );
 }
