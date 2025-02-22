ed7e58bbbfdae7d88e67ea0f9c2556a363826ac8
Mathijs-04 <mathijsvandermeijde@gmail.com>
2025-02-22 14:47:44 +0100

    Updated navigation

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,9 +3,6 @@ import {Link, Outlet} from "react-router";
 function Layout() {
     return (
         <>
-            <header>
-                <h1>Portfolio</h1>
-            </header>
             <nav>
                 <Link to={"/"}>Home</Link>
                 <Link to={"/projects"}>Projects</Link>
