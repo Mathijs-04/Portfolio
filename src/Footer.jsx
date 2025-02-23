7a922458b351aab727f516bd0f306e67955a43b8
Mathijs-04 <mathijsvandermeijde@gmail.com>
2025-02-23 15:16:43 +0100

    Updated footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-white rounded-lg shadow-sm m-4 dark:bg-gray-800">
-            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                    © 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+        <footer className="bg-gray-900 text-white py-6 px-8">
+            <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row justify-between items-center">
+                <span className="text-sm text-gray-400">
+                    © 2023 Mathijs van der Meijde. All Rights Reserved.
                 </span>
-                <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+
+                <ul className="flex space-x-6 mt-3 md:mt-0">
                     <li>
-                        <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                        <a href="#" className="hover:text-gray-400">About</a>
                     </li>
                     <li>
-                    </li>
-                    <li>
-                        <a href="#" className="hover:underline">Contact</a>
+                        <a href="#" className="hover:text-gray-400">Contact</a>
                     </li>
                 </ul>
             </div>
