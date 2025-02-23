8bfa095e4ab93102bc12bf235384af42487f22fa
Mathijs-04 <mathijsvandermeijde@gmail.com>
2025-02-23 15:16:37 +0100

    Updated nav

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,19 +1,23 @@
-import {Link, Outlet} from "react-router";
+import { Link, Outlet } from "react-router";
 
 function Layout() {
     return (
         <>
-            <nav>
-                <Link to={"/"}>Home</Link>
-                <Link to={"/projects"}>Projects</Link>
-                <Link to={"/experience"}>Experience</Link>
-                <Link to={"/about"}>About</Link>
+            <nav className="flex justify-between items-center px-8 py-4 bg-gray-900 text-white">
+                <h1 className="text-lg font-semibold">Mathijs van der Meijde</h1>
+
+                <div className="flex space-x-6">
+                    <Link to="/" className="hover:text-gray-400">Home</Link>
+                    <Link to="/projects" className="hover:text-gray-400">Projects</Link>
+                    <Link to="/experience" className="hover:text-gray-400">Experience</Link>
+                    <Link to="/about" className="hover:text-gray-400">About</Link>
+                </div>
             </nav>
             <main>
-                <Outlet/>
+                <Outlet />
             </main>
         </>
-    )
+    );
 }
 
 export default Layout;
