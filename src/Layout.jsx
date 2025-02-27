5e49107d56c5eebf1afeb4f951e499df9148ad13
Mathijs-04 <mathijsvandermeijde@gmail.com>
2025-02-27 18:53:37 +0100

    Updated nav styling

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,20 +1,18 @@
-import { Link, Outlet } from "react-router";
+import { NavLink, Outlet } from "react-router";
 
 function Layout() {
     return (
         <>
-            <nav className="flex justify-between items-center px-8 py-4 bg-gray-900 text-white">
-                <h1 className="text-lg font-semibold">Mathijs van der Meijde</h1>
-
-                <div className="flex space-x-6 pr-4">
-                    <Link to="/" className="hover:text-gray-400">Home</Link>
-                    <Link to="/projects" className="hover:text-gray-400">Projects</Link>
-                    <Link to="/experience" className="hover:text-gray-400">Experience</Link>
-                    <Link to="/about" className="hover:text-gray-400">About</Link>
+            <nav className="bg-gray-900 text-white shadow font-panchang font-semibold">
+                <div className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+                    <NavLink to="/" className={({ isActive }) => isActive ? "text-gray-800 dark:text-gray-200 border-b-2 border-[#60A5FA] mx-1.5 sm:mx-6" : "border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-[#60A5FA] mx-1.5 sm:mx-6"}>Home</NavLink>
+                    <NavLink to="/projects" className={({ isActive }) => isActive ? "text-gray-800 dark:text-gray-200 border-b-2 border-[#60A5FA] mx-1.5 sm:mx-6" : "border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-[#60A5FA] mx-1.5 sm:mx-6"}>Projects</NavLink>
+                    <NavLink to="/experience" className={({ isActive }) => isActive ? "text-gray-800 dark:text-gray-200 border-b-2 border-[#60A5FA] mx-1.5 sm:mx-6" : "border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-[#60A5FA] mx-1.5 sm:mx-6"}>Experience</NavLink>
+                    <NavLink to="/about" className={({ isActive }) => isActive ? "text-gray-800 dark:text-gray-200 border-b-2 border-[#60A5FA] mx-1.5 sm:mx-6" : "border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-[#60A5FA] mx-1.5 sm:mx-6"}>About</NavLink>
                 </div>
             </nav>
             <main>
-                <Outlet/>
+                <Outlet />
             </main>
         </>
     );
