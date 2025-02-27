ec5f89ca7a261606002449fd706dd8853868087a
Mathijs-04 <mathijsvandermeijde@gmail.com>
2025-02-27 19:19:51 +0100

    Fixed nav bug

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -5,10 +5,10 @@ function Layout() {
         <>
             <nav className="bg-gray-900 text-white shadow font-panchang font-semibold gradient-underline-nav">
                 <div className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
-                    <NavLink to="/" className={({ isActive }) => isActive ? "text-gray-800 dark:text-white mx-1.5 sm:mx-6" : "border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-[#60A5FA] mx-1.5 sm:mx-6"}>Home</NavLink>
-                    <NavLink to="/projects" className={({ isActive }) => isActive ? "text-gray-800 dark:text-white mx-1.5 sm:mx-6" : "border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-[#60A5FA] mx-1.5 sm:mx-6"}>Projects</NavLink>
-                    <NavLink to="/experience" className={({ isActive }) => isActive ? "text-gray-800 dark:text-white mx-1.5 sm:mx-6" : "border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-[#60A5FA] mx-1.5 sm:mx-6"}>Experience</NavLink>
-                    <NavLink to="/about" className={({ isActive }) => isActive ? "text-gray-800 dark:text-white mx-1.5 sm:mx-6" : "border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-[#60A5FA] mx-1.5 sm:mx-6"}>About</NavLink>
+                    <NavLink to="/" className={({ isActive }) => isActive ? "nav-link active mx-1.5 sm:mx-6" : "nav-link mx-1.5 sm:mx-6"}>Home</NavLink>
+                    <NavLink to="/projects" className={({ isActive }) => isActive ? "nav-link active mx-1.5 sm:mx-6" : "nav-link mx-1.5 sm:mx-6"}>Projects</NavLink>
+                    <NavLink to="/experience" className={({ isActive }) => isActive ? "nav-link active mx-1.5 sm:mx-6" : "nav-link mx-1.5 sm:mx-6"}>Experience</NavLink>
+                    <NavLink to="/about" className={({ isActive }) => isActive ? "nav-link active mx-1.5 sm:mx-6" : "nav-link mx-1.5 sm:mx-6"}>About</NavLink>
                 </div>
             </nav>
             <main>
