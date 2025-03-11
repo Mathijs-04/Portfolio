c335df6ecfddf3ee543369f8422310c22c121d12
Mathijs-04 <mathijsvandermeijde@gmail.com>
2025-03-11 13:28:27 +0100

    Updated buttons and about text

--- src/Home.jsx
+++ src/Home.jsx
@@ -49,7 +49,7 @@ function Home() {
                             className="group relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg
                             bg-gradient-to-br from-[#6C5CE7] via-[#A29BFE] to-[#D6CCFF]
                             group-hover:from-[#6C5CE7] group-hover:via-[#A29BFE] group-hover:to-[#D6CCFF]
-                            hover:text-white dark:text-white focus:ring-4 focus:outline-none     focus:ring-purple-500"
+                            hover:text-white dark:text-white"
                         >
                             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md
                                 group-hover:bg-transparent group-hover:dark:bg-transparent">
