30cc54786fdafe7e96414f4b509476bcaaac7bb8
Mathijs-04 <mathijsvandermeijde@gmail.com>
2025-03-14 11:49:41 +0100

    Updated home page buttons

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,8 +9,7 @@ function Home() {
             <div className="text-center text-white">
                 <h1 className="text-6xl font-panchang font-bold">Mathijs van der Meijde</h1>
 
-                <div id="typewriter"
-                     className="mt-4 text-4xl text-white font-panchang font-semibold text-left overflow-hidden">
+                <div id="typewriter" className="mt-4 text-4xl text-white font-panchang font-semibold text-left overflow-hidden">
                     <Typewriter
                         options={{
                             loop: true,
@@ -39,24 +38,19 @@ function Home() {
                     />
                 </div>
 
-                <br/>
+                <br />
 
-                <div className="mt-4">
+                <div className="mt-4 space-x-4">
                     {["projects", "experience", "about"].map((page) => (
                         <button
                             key={page}
                             onClick={() => navigate(`/${page}`)}
-                            className="group relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg
-                            bg-gradient-to-br from-[#6C5CE7] via-[#A29BFE] to-[#D6CCFF]
-                            group-hover:from-[#6C5CE7] group-hover:via-[#A29BFE] group-hover:to-[#D6CCFF]
-                            hover:text-white dark:text-white"
+                            className="group relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg bg-gradient-to-br from-[#6C5CE7] via-[#A29BFE] to-[#D6CCFF] group-hover:from-[#6C5CE7] group-hover:via-[#A29BFE] group-hover:to-[#D6CCFF] hover:text-white dark:text-white w-48 flex-shrink-0"
                         >
-                            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md
-                                group-hover:bg-transparent group-hover:dark:bg-transparent">
+                            <span className="relative px-10 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent w-full">
                                 {page === "projects" ? "My Projects" : page === "experience" ? "My Experience" : "About Me"}
                             </span>
-                            <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-12deg)_translateX(-100%)]
-                                group-hover:duration-1000 group-hover:[transform:skew(-12deg)_translateX(100%)]">
+                            <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-12deg)_translateX(-100%)] group-hover:duration-1000 group-hover:[transform:skew(-12deg)_translateX(100%)]">
                                 <div className="relative h-full w-8 bg-white/20"></div>
                             </div>
                         </button>
