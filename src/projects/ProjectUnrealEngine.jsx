b290bc0897c46c0edbda044def5da4dbd010b046
Mathijs-04 <mathijsvandermeijde@gmail.com>
2025-03-16 12:01:11 +0100

    Updated ProjectUnrealEngine.jsx

--- src/projects/ProjectUnrealEngine.jsx
+++ src/projects/ProjectUnrealEngine.jsx
@@ -3,7 +3,7 @@ import { SiUnrealengine } from "react-icons/si";
 import CarouselComponent from "../ProjectCarousel.jsx";
 
 function ProjectUnrealEngine() {
-    const carouselImages = ['/unreal-engine.webp', '/unreal-engine.webp', '/unreal-engine.webp'];
+    const carouselImages = ['/unreal-engine.webp', '/unreal-engine-2.webp'];
 
     return (
         <div className="gradient-background min-h-screen">
