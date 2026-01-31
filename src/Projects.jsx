5187d8ed3088ca474e9f9c4bc93b2cec70dae89a
Mathijs-04 <mathijsvandermeijde@gmail.com>
2026-01-31 16:06:50 +0100

    Updated project order

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -125,6 +125,14 @@ const projects = [
         hoverClass: "hover:shadow-[0_0_15px_5px_rgba(96,165,250,0.6)] hover:border-blue-400"
     },
     {
+        name: "The Adventurers Armory",
+        slug: "adventurers-armory",
+        description: "A Fantasy Store Experience",
+        image: "/Portfolio/armory.webp",
+        tech: [FaVuejs],
+        hoverClass: "hover:shadow-[0_0_15px_5px_rgba(96,165,250,0.6)] hover:border-blue-400"
+    },
+    {
         name: "WARHAMMER Dice Roller",
         slug: "dice-roller",
         description: "The online dice rolling tool for Wargaming enthusiasts",
@@ -178,14 +186,6 @@ const projects = [
         description: "A fictional company website specialized in VR experiences",
         image: "/Portfolio/exp-corp.webp",
         tech: [FaReact, SiTailwindcss, FaDatabase],
-        hoverClass: "hover:shadow-[0_0_15px_5px_rgba(96,165,250,0.6)] hover:border-blue-400"
-    },
-    {
-        name: "The Adventurers Armory",
-        slug: "adventurers-armory",
-        description: "A Fantasy Store Experience",
-        image: "/Portfolio/armory.webp",
-        tech: [FaVuejs],
         hoverClass: "hover:shadow-[0_0_15px_5px_rgba(96,165,250,0.6)] hover:border-blue-400"
     },
     // {
